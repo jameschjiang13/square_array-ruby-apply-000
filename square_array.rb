37af100ca25f798282f49586a9efba7dd26e015d
James Jiang <monochrome473@gmail.com>
2020-03-27 02:23:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  array.each do |number|
+    counter += 1
+    square = number * number
+    array [counter] = square
+  end
 end
+
+square_array([1, 2, 3])
